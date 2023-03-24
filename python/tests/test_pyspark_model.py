812a929e84c027611b10d45df17ff22fcf0b77a4
Bobby Wang <wbo4958@gmail.com>
2023-03-25 07:31:12 +0800

    update

---
+++ python/tests/test_pyspark_model.py
@@ -0,0 +1,68 @@
+
+
+from python.tests.sparksession import CleanSparkSession
+
+
+def test_ml_kmeans_model():
+    from pyspark.mllib.clustering import KMeansModel as MllibKMeansModel
+    from pyspark.mllib.common import _py2java
+    from pyspark.mllib.linalg import _convert_to_vector
+    from pyspark.ml.linalg import Vectors
+
+    from spark_rapids_ml.clustering import KMeans as CumlKMeans
+
+    with CleanSparkSession() as spark:
+        data = [(Vectors.dense([0.0, 0.0]), 2.0), (Vectors.dense([1.0, 1.0]), 2.0),
+                (Vectors.dense([9.0, 8.0]), 2.0), (Vectors.dense([8.0, 9.0]), 2.0)]
+        df = spark.createDataFrame(data, ["features", "weighCol"])
+        sc = spark.sparkContext
+
+        centers = [[0.5, 0.5], [8.5, 8.5]]
+        cuml_model = CumlKMeans().fit(df)
+
+        centers = cuml_model.cluster_centers_
+
+        # kmeans_mllib_model = MllibKMeansModel(centers)
+
+        java_centers = _py2java(sc, [_convert_to_vector(c) for c in centers])
+        java_model = sc._jvm.org.apache.spark.mllib.clustering.KMeansModel(java_centers)
+
+        final_model = sc._jvm.org.apache.spark.ml.clustering.KMeansModel("xxx", java_model)
+
+        from pyspark.ml.clustering import KMeansModel as MLKmeansModel
+        kmeans_model = MLKmeansModel(final_model)
+        kmeans_model.transform(df).show(100, False)
+        kmeans_model.write().overwrite().save("/tmp/xxxx")
+
+
+
+def test_lr_model():
+    from spark_rapids_ml.regression import LinearRegression as CumlLinearRegression
+    from pyspark.ml.regression import LinearRegressionModel as LRModel
+    from pyspark.ml.linalg import Vectors
+    from pyspark.ml.common import _py2java
+    from pyspark.ml.linalg import _convert_to_vector
+
+    with CleanSparkSession() as spark:
+        df = spark.createDataFrame([
+            (1.0, 2.0, Vectors.dense(1.0, 2.0)),
+            (0.0, 2.0, Vectors.dense(3.2, 7.2))], ["label", "weight", "features"])
+        lr = CumlLinearRegression(regParam=0.0, solver="normal")
+        model = lr.fit(df)
+        intercept = float(model.intercept)
+        coef = _convert_to_vector(model.coefficients)
+        print(coef)
+
+        sc = spark.sparkContext
+        assert sc._jvm is not None
+
+        java_model = sc._jvm.org.apache.spark.ml.regression.LinearRegressionModel(
+            "xyx",
+            _py2java(sc, coef), intercept, 0.2)
+
+        final_model = LRModel(java_model)
+        final_model.transform(df).show()
+        # final_model.write().overwrite().save("/tmp/zzzzzz")
+
+        # model.write().overwrite().save("/tmp/xyz")
+        # model.transform(df).show()
